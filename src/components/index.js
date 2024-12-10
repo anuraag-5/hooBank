9274108434ba243b006ad8e6477367dd262dc10a
Anurag bhoite <anuragcr7778@gmail.com>
2024-12-10 16:13:21 +0530

    Done

---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import NavBar from "./NavBar";
+import Billing from "./Billing";
+import Business from "./Business";
+import Button from "./Button"
+import CardDeal from "./CardDeal"
+import ClientSection from "./ClientSection"
+import CTA from "./CTA"
+import Feedback from "./Feedback"
+import Footer from "./Footer"
+import Hero from "./Hero"
+import Stats from "./Stats"
+import Testimonials from "./Testimonials"
+import GetStarted from "./GetStarted"
+
+export {
+    NavBar,
+    Billing,
+    Business,
+    Button,
+    CardDeal,
+    ClientSection,
+    CTA,
+    Feedback,
+    Footer,
+    Hero,
+    Stats,
+    Testimonials,
+    GetStarted
+}
